b2901c74ed08cf663ad8d1c8469d3daaed371876
TechCypher <connorhardy42@gmail.com>
2018-01-05 19:25:32 +0000

    New Area and bug fixes
    Changes:
    - Changed Camera clear flag option to don't clear in an attempt to stop map tearing
    - Fixed player speed
    - Player now shows on main camrea
    -
    
    Added:
    - Coordiante update script for the minimap coordinates
    - New Area
    
    Removed:
    - Camera Control Class
    
    Need To Fix:
    - Pause Menu
    - Make boundaries so that the map doent mess up

---
+++ Adventure_RPG/Assets/Scripts/UpdateCoordinates.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateCoordinates : MonoBehaviour
+{
+	public GameObject player;
+	public Text text;
+
+	private float x, y;
+	private bool isPlayerMoving;
+
+	void Start () 
+	{
+		isPlayerMoving = PlayerControl.isPlayerMoving;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+		x = player.transform.position.x;
+		y = player.transform.position.y;
+
+		x = (float)Math.Round (x, 2);
+		y = (float)Math.Round (y, 2);
+		//transform.gameObject.GetComponent<Text>().text = ("[ " + x + "," + y + " ]");
+		text.text = ("[ " + x + "," + y + " ]");
+	}
+}
